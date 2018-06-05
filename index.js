65b6957d1e172bd1e178b2b7c6e0a2f5a61a9237
Sindre Sorhus <sindresorhus@gmail.com>
2018-06-05 14:32:04 +0700

    Use an actual `Map`

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 'use strict';
 const os = require('os');
 
-const nameMap = {
-	18: 'Mojave',
-	17: 'High Sierra',
-	16: 'Sierra',
-	15: 'El Capitan',
-	14: 'Yosemite',
-	13: 'Mavericks',
-	12: 'Mountain Lion',
-	11: 'Lion',
-	10: 'Snow Leopard',
-	9: 'Leopard',
-	8: 'Tiger',
-	7: 'Panther',
-	6: 'Jaguar',
-	5: 'Puma'
-};
+const nameMap = new Map([
+	[18, 'Mojave'],
+	[17, 'High Sierra'],
+	[16, 'Sierra'],
+	[15, 'El Capitan'],
+	[14, 'Yosemite'],
+	[13, 'Mavericks'],
+	[12, 'Mountain Lion'],
+	[11, 'Lion'],
+	[10, 'Snow Leopard'],
+	[9, 'Leopard'],
+	[8, 'Tiger'],
+	[7, 'Panther'],
+	[6, 'Jaguar'],
+	[5, 'Puma']
+]);
 
 module.exports = release => {
-	release = (release || os.release()).split('.')[0];
+	release = Number((release || os.release()).split('.')[0]);
 	return {
-		name: nameMap[release],
-		version: '10.' + (Number(release) - 4)
+		name: nameMap.get(release),
+		version: '10.' + (release - 4)
 	};
 };
