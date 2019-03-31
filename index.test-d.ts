6535ee9168b1f15e9d30860d771496c6f6e075ab
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-03-31 12:28:47 +0000

    Refactor TypeScript definition to CommonJS compatible export (#4)

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,5 +1,5 @@
-import {expectType} from 'tsd-check';
-import macosRelease from '.';
+import {expectType} from 'tsd';
+import macosRelease = require('.');
 
 expectType<string>(macosRelease());
 expectType<string>(macosRelease('foo'));
